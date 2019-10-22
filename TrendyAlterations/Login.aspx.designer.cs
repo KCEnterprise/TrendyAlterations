51b503b70d20d138af36663655643743ad9dd7d7
Kehli Cousins <Kehli Cousins@KMC-HP2019>
2019-10-21 21:52:14 -0500

    Login form

--- TrendyAlterations/Login.aspx.designer.cs
+++ TrendyAlterations/Login.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace TrendyAlterations
 {
 
 
-    public partial class WebForm1
+    public partial class WebForm3
     {
     }
 }
--- TrendyAlterations/Site1.Master.designer.cs
+++ TrendyAlterations/Site1.Master.designer.cs
@@ -40,5 +40,14 @@ namespace TrendyAlterations
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image Image1;
+
+        /// <summary>
+        /// MainContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
     }
 }
